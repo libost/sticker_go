9a48069f5334893064b1910ccbe75f99c448e3b1
libost <admin@libost.xyz>
2026-04-06 23:06:31 +0800

    fix: error on initCase() at database.go

--- database/database.go
+++ database/database.go
@@ -158,7 +158,7 @@ func logIntoDonateLogsSuccess(conn *sql.DB, id int64, payload string, telegramCh
 }
 
 func initCase(id int64, conn *sql.DB) (map[string]any, error) {
-	data := map[string]any{}
+	data := map[string]any{"user_id": id, "exists": false}
 	// 从这里往下都是兼容性处理，确保即使之前的版本没有 language_code 字段也能正常使用，并且在访问时能够正确返回默认值。
 	rows, err := conn.Query("PRAGMA table_info(USERPOOL)")
 	if err != nil {
