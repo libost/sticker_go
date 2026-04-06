0d543021828cb529f5fea256749afc90127077a0
libost <admin@libost.xyz>
2026-04-06 20:33:50 +0800

    chore: refactor

--- utils/subscribe.go
+++ utils/subscribe.go
@@ -3,21 +3,27 @@ package utils
 import (
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/libost/sticker_go/config"
 	C "github.com/libost/sticker_go/constants"
+	I "github.com/libost/sticker_go/i18n"
 	"github.com/libost/sticker_go/log"
 
 	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
 var ErrUserNotSubscribed = errors.New("user is not subscribed")
 
-func SubscribeCheck(b *gotgbot.Bot, uid int64) error {
+func SubscribeCheck(b *gotgbot.Bot, ctx *ext.Context, uid int64, langCode string) error {
 	cf, err := config.Init()
 	if err != nil {
 		log.Log(fmt.Sprintf("User %d failed to load config for subscription check: %v", uid, err), C.LogLevelError)
 		return err
+	}
+	if !cf.Subscription.Enabled {
+		return nil // 订阅检查未启用，直接通过
 	}
 	if cf.Subscription.Channel == "" {
 		log.Log(fmt.Sprintf("User %d failed to check subscription: channel is empty", uid), C.LogLevelError)
@@ -33,11 +39,27 @@ func SubscribeCheck(b *gotgbot.Bot, uid int64) error {
 	})
 	if err != nil {
 		log.Log(fmt.Sprintf("User %d failed to check subscription: %v", uid, err), C.LogLevelError)
+		channel := strings.TrimPrefix(cf.Subscription.Channel, "@")
+		displayText := fmt.Sprintf(I.GetLocalisedString("general.subscription_check_failed", langCode), channel, cf.Subscription.Channel)
+		_, replyErr := ctx.EffectiveMessage.Reply(b, displayText, &gotgbot.SendMessageOpts{
+			ParseMode: "HTML",
+		})
+		if replyErr != nil {
+			log.Log(fmt.Sprintf("User %d failed to send subscription message: %v", uid, replyErr), C.LogLevelError)
+		}
 		return err
 	}
 	member := result.MergeChatMember()
 	if member.Status != "left" && member.Status != "kicked" {
 		return nil // 用户已订阅
+	}
+	channel := strings.TrimPrefix(cf.Subscription.Channel, "@")
+	displayText := fmt.Sprintf(I.GetLocalisedString("general.subscription_required", langCode), channel, cf.Subscription.Channel)
+	_, replyErr := ctx.EffectiveMessage.Reply(b, displayText, &gotgbot.SendMessageOpts{
+		ParseMode: "HTML",
+	})
+	if replyErr != nil {
+		log.Log(fmt.Sprintf("User %d failed to send subscription check failed message: %v", uid, replyErr), C.LogLevelError)
 	}
 	return ErrUserNotSubscribed
 }
